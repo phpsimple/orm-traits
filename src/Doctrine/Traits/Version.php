344531fb0e13517534ba8b28c43e3f67f3657464
api-platform-tools <simpletoimplement+apt@gmail.com>
2023-10-22 02:19:04 +0300

    chore: ignore version in serializer

--- src/Doctrine/Traits/Version.php
+++ src/Doctrine/Traits/Version.php
@@ -3,11 +3,13 @@
 namespace PhpSimple\Doctrine\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 trait Version
 {
     #[ORM\Column(type: 'integer')]
     #[ORM\Version]
+    #[Ignore]
     protected ?int $version = null;
 
     public function getVersion(): ?int
